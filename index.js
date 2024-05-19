7952e52c53f57df96c32db1a49fb22ae658db4e7
Luiz Jacson <luiz.jacson@souunit.com.br>
2024-05-19 17:54:09 -0300

    yaml

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const swaggerUi = require('swagger-ui-express')
 const PORT = process.env.PORT || 3000; 
 const conteudoRoutes = require('./routes/conteudoRoutes')
 const instrucaoRoutes = require('./routes/instrucaoRoutes')
 const genericoRoutes = require('./routes/genericoRoutes')
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('swagger.yaml');
-app.use(express.json())
 
-app.use('/inserirInstrucao', instrucaoRoutes)
-app.use('/gerarConteudoEspecifico', conteudoRoutes)
-app.use('/gerarConteudoGenerico', genericoRoutes)
+fs.readFile('./swagger.yaml', 'utf8', (err, data) => {
+    if (err) {
+        console.error('Erro ao carregar o arquivo YAML:', err);
+        return;
+    }
 
-app.use('/api-TinBolt-GEMINI', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    const swaggerDocument = YAML.parse(data);
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    app.use(express.json())
+
+    app.use('/inserirInstrucao', instrucaoRoutes)
+    app.use('/gerarConteudoEspecifico', conteudoRoutes)
+    app.use('/gerarConteudoGenerico', genericoRoutes)
+
+    app.use('/api-TinBolt-GEMINI', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
 });
